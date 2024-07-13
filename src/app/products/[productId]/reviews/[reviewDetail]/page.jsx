5b6e405a2650a29ad387b3086e80cf0a8b163baa
Shantanu <shantanusaini7000@gmail.com>
2024-07-13 22:11:06 +0530

    nested routing

---
+++ src/app/products/[productId]/reviews/[reviewDetail]/page.jsx
@@ -0,0 +1,7 @@
+export default function ReviewDetails({ params }) {
+    return (
+        <>
+            <h1>this is the review of product {params.productId} and review no. is {params.reviewDetail}</h1>
+        </>
+    )
+}
