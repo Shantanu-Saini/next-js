a2efc6c36f358921f0b224f8e6990adcd67ab858
Shantanu <shantanusaini7000@gmail.com>
2024-07-13 20:35:59 +0530

    dynamic routing

---
+++ src/app/products/[productId]/page.jsx
@@ -0,0 +1,5 @@
+export default function ProductDetails({ params }) {
+    return (
+        <h1>This is deatail of product {params.productId}</h1>
+    )
+}
---
+++ src/app/products/page.jsx
@@ -0,0 +1,7 @@
+export default function Products() {
+    return (
+        <>
+            <h1>This is Products page</h1>
+        </>
+    )
+}
