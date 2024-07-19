c03ccdb8a02c187b644c2309cbd7a5b3f45d6992
Shantanu <shantanusaini7000@gmail.com>
2024-07-19 21:21:49 +0530

    active link styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,36 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
 
 function Header() {
+
+  const pathname = usePathname();
+  const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "Profile", href: "/profile" },
+    { name: "Products", href: "/products" },
+  ]
+
   return (
     <div>
       <header className='grid place-content-center bg-orange-300 py-6'>
         <h1 className='text-4xl font-bold'>My Header</h1>
         <div className='flex items-center justify-center'>
-          <Link href='/' className="text-2xl mx-4">Home</Link>
-          <Link href='/about' className="text-2xl mx-4">About</Link>
-          <Link href='/profile' className="text-2xl mx-4">Profile</Link>
-          <Link href='/products' className="text-2xl mx-4">Products</Link>
+
+          {
+            navLinks.map((link) => {
+              const isActive = link.href === "/" ? pathname === link.href : pathname.startsWith(link.href);
+              return (
+                <Link key={link.name} href={link.href} className={isActive ? `font-bold mx-4` : `mx-4 italic`}>{link.name}</Link>
+              )
+            })
+          }
         </div>
       </header>
     </div>
   )
 }
 
-export default Header
+export default Header;
