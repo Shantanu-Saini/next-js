16b8328d542f9f9218d528e62c792b9c70847bca
Shantanu <shantanusaini7000@gmail.com>
2024-07-21 21:49:01 +0530

    loading page

---
+++ src/app/profile/loading.jsx
@@ -0,0 +1,9 @@
+export default function loading() {
+  return (
+    <>
+      <div className="bg-violet-800 h-screen w-screen">
+        <h1 className="text-white text-3xl">Loading....</h1>
+      </div>
+    </>
+  )
+}
