af122aa2685798c0d06038c5a31fbf48ae427963
Shantanu <shantanusaini7000@gmail.com>
2024-07-27 23:09:32 +0530

    add loading screen on parallel routes

---
+++ src/app/dashboard/@notification/loading.jsx
@@ -0,0 +1,10 @@
+export default function loading() {
+    return (
+      <>
+        <div className="bg-red-800 h-screen w-screen">
+          <h1 className="text-white text-3xl">Loading....</h1>
+        </div>
+      </>
+    )
+  }
+  
