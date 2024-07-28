7361f24be99cfb25494646fe215e1d656ab60504
Shantanu <shantanusaini7000@gmail.com>
2024-07-28 23:10:04 +0530

    sub-navigation

--- src/app/dashboard/@notification/history/page.jsx
+++ src/app/dashboard/@notification/history/page.jsx
@@ -1,12 +1,12 @@
 import Card from "@/components/Card"
 import Link from "next/link"
 
-export default function Notification() {
+export default function NotificationHistory() {
     return (
         <>
             <div className="h-full flex flex-col justify-between items-center">
-                <Card>Notification</Card>
-                <Link href="/dashboard/history">Notification history</Link>
+                <Card>Notification History</Card>
+                <Link href="/dashboard">Notification</Link>
             </div>
         </>
 
