e116e684cfcb393d31adca08f89a04023188cd55
Shantanu <shantanusaini7000@gmail.com>
2024-07-28 23:24:21 +0530

    unmatched routes

---
+++ src/app/dashboard/@business/default.jsx
@@ -0,0 +1,7 @@
+import Card from "@/components/Card";
+
+export default function DefaultBusiness() {
+  return (
+    <Card>Business</Card>
+  )
+}
---
+++ src/app/dashboard/default.jsx
@@ -0,0 +1,5 @@
+export default function DefaultDashboard() {
+    return (
+        <h1 className="font-bold text-5xl">User Dashboard</h1>
+    )
+}
