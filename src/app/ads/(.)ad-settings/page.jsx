40aa895e56ac22479acca836650b737e6d30a445
Shantanu <shantanusaini7000@gmail.com>
2024-07-31 23:49:39 +0530

    intercepting routes on same level

---
+++ src/app/ads/(.)ad-settings/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function InterceptedAdSettings() {
+  return (
+    <div className="flex flex-col items-center justify-center py-4 space-y-3">
+        <h1 className="text-5xl font-bold">Intercepted Ads Setting Page</h1>
+        <Link href="/ads" className="underline text-blue-700">Go To Ads page</Link>
+    </div>
+  )
+}
---
+++ src/app/ads/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function AdsPage() {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="text-5xl font-bold my-4">Ads page</h1>
+            <Link href="/ads/ad-settings" className="underline text-blue-700">Ad Settings</Link>
+        </div>
+    )
+}
